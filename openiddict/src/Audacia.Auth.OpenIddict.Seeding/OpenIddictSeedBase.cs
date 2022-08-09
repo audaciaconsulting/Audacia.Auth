fe969d5e871427f0551d31160894b21d75bea62b
Richard Brown <richard.brown@audacia.co.uk>
2022-08-09 13:39:49 +0100

    feat: don't require implementation of IOpenIdConnectConfigMapper #119446

--- openiddict/src/Audacia.Auth.OpenIddict.Seeding/OpenIddictSeedBase.cs
+++ openiddict/src/Audacia.Auth.OpenIddict.Seeding/OpenIddictSeedBase.cs
@@ -74,12 +74,19 @@ namespace Audacia.Auth.OpenIddict.Seeding
                 .ToArray();
             if (!mapperTypes.Any())
             {
-                throw new InvalidOperationException($"The project {_identityProjectName} does not contain an implementation of '{nameof(IOpenIdConnectConfigMapper)}'.");
+                // No implementation of IOpenIdConnectConfigMapper, so try and get the config object directly
+                var openIdConnectConfig = configuration.GetSection("OpenIdConnectConfig").Get<OpenIdConnectConfig>();
+                if (openIdConnectConfig is null)
+                {
+                    throw new InvalidOperationException($"Either an implementation of '{nameof(IOpenIdConnectConfigMapper)}' must be provided in project {_identityProjectName}, or an 'OpenIdConnectConfig' section must be present in configuration that maps to an object of type '${nameof(OpenIdConnectConfig)}'.");
+                }
+
+                return openIdConnectConfig;
             }
 
             if (Activator.CreateInstance(mapperTypes.First()) is not IOpenIdConnectConfigMapper mapper)
             {
-                throw new InvalidOperationException($"The project {_identityProjectName} does not contain an implementation of '{nameof(IOpenIdConnectConfigMapper)}' with a parameterless constructor.");
+                throw new InvalidOperationException($"The implementation of '{nameof(IOpenIdConnectConfigMapper)}' in project {_identityProjectName} must have a parameterless constructor.");
             }
 
             return mapper.Map(configuration);
