12a02cc75735d453a6456cab8efcd23d5349e519
tom.shephard <tom.shephard@audacia.co.uk>
2024-12-11 11:25:52 +0000

    test

--- openiddict/tests/Audacia.Auth.OpenIddict.Tests/Common/Configuration/OpenIdConnectConfigTests.cs
+++ openiddict/tests/Audacia.Auth.OpenIddict.Tests/Common/Configuration/OpenIdConnectConfigTests.cs
@@ -9,21 +9,6 @@ public class OpenIdConnectConfigTests
     private readonly OpenIdConnectConfig _config = new OpenIdConnectConfig();
 
     [Fact]
-    public void All_clients_contains_client_credentials_clients()
-    {
-        const string clientId = "api-client";
-        _config.ClientCredentialsClients = new[]
-        {
-            new ClientCredentialsClient { ClientId = clientId }
-        };
-
-        var allClients = _config.AllClients;
-
-        allClients.Should().Contain(client => client.ClientId == clientId);
-    }
-
-    [Fact]
-    public void All_clients_contains_authorization_code_clients()
     {
         const string clientId = "ui-client";
         _config.AuthorizationCodeClients = new[]
